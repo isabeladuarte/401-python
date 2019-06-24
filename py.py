1e172518953f8680d6bc77d8a8eebc5e51bf1047
Isabela <isabeladuartelbg@gmail.com>
2019-06-24 16:57:29 -0300

    Exericios de Python

---
+++ py.py
@@ -0,0 +1,33 @@
+# n1 = int (input ("Digite nota 1: "))
+# n2 = int (input ("Digite nota 2: "))
+# n3 = int (input ("Digite nota 3: "))
+# n4 = int (input ("Digite nota 4: "))
+
+# media = ((n1+n2+n3+n4)/4)
+# print ("A média é {}".format(media))
+
+
+# palavra = input ("Digite a palavra: ")
+# letra = input ("Digite a letra: ")
+# x = palavra.count(letra)
+# print (x)
+
+
+# numeros = int (input("Digite quantos números terá na média: "))
+# soma = 0
+# for i in range (numeros):
+#     soma += int (input("Digite a sua nota: "))
+   
+# media = soma/numeros
+# print("A média é {}".format(media))
+
+
+# num1 = int (input("Digite o primeiro número: "))
+# num2 = int (input("Digite o segundo número: "))
+
+# print ("A soma é: {}".format(num1+num2))
+# print ("A subtração é: {}".format(num1-num2))
+# print ("A multiplicação é: {}".format(num1*num2))
+# print ("A divisão é: {}".format(num1/num2))
+
+
